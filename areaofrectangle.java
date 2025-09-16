99fbfd5790f9841b54b5fa5a64a72baac85459ad
Manivas <manivaschalla@gmail.com>
2025-09-16 18:34:13 +0530

    Commitimg first code for logic building

---
+++ areaofrectangle.java
@@ -0,0 +1,11 @@
+
+public class areaofrectangle {
+    public static void main(String[] args){
+
+    
+    int l = 10;
+    int w = 5;
+    int area = l * w;
+    System.out.println(area);
+}
+}
---
+++ ascii.java
@@ -0,0 +1,13 @@
+
+//Method 1
+//Simply assigning the character to the integer variable.
+//Java automatically stores the ascii value.
+
+public class ascii {
+    public static void main(String args[]){
+        int assc = '}';
+        System.out.println("The ascii value is : "+assc);
+    }
+}
+
+//Method 2
