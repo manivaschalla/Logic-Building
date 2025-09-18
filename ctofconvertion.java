40cdf1053d3d0d9daaafae2a7f58ad9b012482c7
Manivas <manivaschalla@gmail.com>
2025-09-18 16:41:29 +0530

    Third problem - convertion of celsius to fahrenheit

---
+++ ctofconvertion.java
@@ -0,0 +1,7 @@
+public class ctofconvertion {
+    public static void main(String args[]){
+        float celsius = 37.5f;
+        float fahrenheit = (celsius * 9/5) + 32;
+        System.out.println(celsius + " degree celsius is equal to " + fahrenheit + " degree fahrenheit.");
+    }
+}
