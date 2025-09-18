2757d5e78509ba956f5453bf408414276fe745fa
Manivas <manivaschalla@gmail.com>
2025-09-18 17:17:31 +0530

    Swapping of two values

---
+++ swapvalues.java
@@ -0,0 +1,10 @@
+public class swapvalues {
+    public static void main(String[] args){
+        int a = 5, b = 10;
+        System.out.println("Before Swapping: A = " + a + "B = "+ b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After Swapping: A = " + a + "n = " +b);
+        }
+}
