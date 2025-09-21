5b4d2e487fdf4b1fee14b90c39ff1294aaf5be8e
Manivas <manivaschalla@gmail.com>
2025-09-21 15:19:05 +0530

    Finding given number is EVEN or ODD

---
+++ evenorodd.java
@@ -0,0 +1,11 @@
+public class evenorodd {
+    public static void main(String args[]){
+        int num  = -153;
+        if (num % 2 == 0){
+            System.out.println("The "+ num +" is an EVEN Number");
+        }else{
+                System.out.println("The "+ num +" is an ODD Number");
+        
+        }
+    }
+}
