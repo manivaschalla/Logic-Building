0ac37626db5377c447560d42b898281c79e6323c
Manivas <manivaschalla@gmail.com>
2025-09-22 21:42:57 +0530

    Checking Given Character is Vowel or Consonant

---
+++ Findingvowel.java
@@ -0,0 +1,12 @@
+
+public class Findingvowel {
+    public static void main(String args[]){
+        char ch = 'b';
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+            System.out.println("The "+ ch + " is an Vowel");
+        }else{
+            System.out.println("The " + ch + " is an consonants");
+        }
+    }
+}
