b002cf38fd53a34a855e98777fee8ec41e3c38ea
Manivas <manivaschalla@gmail.com>
2025-09-26 16:08:35 +0530

    Checking given number is Positive or Ngative

---
+++ Posandneg.java
@@ -0,0 +1,10 @@
+public class Posandneg{
+    public static void main (String args[]){
+        int num = 55;
+        if (num <= 0){
+            System.out.println("Given Number is Negative");
+        }else{
+            System.out.println("The Given Number Is Positive");
+        }
+    }
+}
